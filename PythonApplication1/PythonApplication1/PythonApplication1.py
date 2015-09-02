2edd513aebe10ab5cb0c5e42125dc021bb21570d
HCK12345 <dayflyace2@naver.com>
2015-09-02 13:45:58 +0900

    9/2

---
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,43 @@
+﻿#print("I have %d cats and %d dogs"%(6,77))
+#print('I have %d cats' %6)
+#salary = input("please enter your salary : ")
+#bonus = input("please enter your age : ")
+#Sum = float(salary) + float(bonus) 
+#print(Sum)
+#s=int(input("please enter s : " ))
+#b=int(input("please enter b : " ))
+#S = s + b
+#print(S)
+
+#여러개의줄
+#주석처리할때
+#이걸사용해
+##컨트롤 K + C  해제할땐 K + U 
+#data = 2.45242
+
+#print(round(data,2))
+#print("월급")
+
+#name = "chunkie"
+#print(name[0])
+#print(name[-2])
+#print(name[5])
+
+#ID = "201111326HCK12345"
+#sid = ID[:9]
+#sname = ID[9:]
+#print ( sid + " " + sname) 
+
+#A = "Scarcraft"
+#B = A[:1] + 't' + A[2:]
+#print(B)
+
+#C = "I eat %10s apples."% "ten"
+#T = "I eat %-10s apples." % "ten"
+#print(C)
+#print(T)
+#print(C.find("apple"))
+
+string = input("종료할까요? : " )
+print(string.capitalize())
+print(string.replace("YES" , "yes"))
